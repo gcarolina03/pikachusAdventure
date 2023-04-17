d31cc7e1d16c792fd9f1f11ca5d02360f6c6a34d
G. Carolina Hernandez <carolinahrndez3@gmail.com>
2023-04-17 11:30:43 +0100

    fix code

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-var game = new Game();
+const game = new Game()
 
-//start game
+// start game
 document
-    .querySelector("#start")
-    .addEventListener('click', function (e) {
-        game.start();
-        e.stopPropagation();
-    })
+  .querySelector('#start')
+  .addEventListener('click', function (e) {
+    game.start()
+    e.stopPropagation()
+  })
 
-//--------------credits
+// --------------credits
 document
-    .querySelector('#menu_credits')
-    .addEventListener('click', function (e) {
-        //remove menu and change container background
-        game.menu.style.display = "none";
-        //show credits
-        game.credits.style.display = "block";
-        e.stopPropagation();
-    })
+  .querySelector('#menu_credits')
+  .addEventListener('click', function (e) {
+    // remove menu and change container background
+    game.menu.style.display = 'none'
+    // show credits
+    game.credits.style.display = 'block'
+    e.stopPropagation()
+  })
 
 document
-    .querySelector('#exitCredits')
-    .addEventListener('click', function (e) {
-        //show menu and change container to bg img
-        game.menu.style.display = "flex";
-        //hide credits
-        game.credits.style.display = "none";
-        e.stopPropagation();
-    })
+  .querySelector('#exitCredits')
+  .addEventListener('click', function (e) {
+    // show menu and change container to bg img
+    game.menu.style.display = 'flex'
+    // hide credits
+    game.credits.style.display = 'none'
+    e.stopPropagation()
+  })
--- js/obstacles.js
+++ js/obstacles.js
@@ -1,31 +1,31 @@
-//---------TREES
-function Obstacles(l,t,w,h){
-    this.left = l;
-    this.top = t;
-    this.width = w;
-    this.height = h;
+// ---------TREES
+function Obstacles (l, t, w, h) {
+  this.left = l
+  this.top = t
+  this.width = w
+  this.height = h
 }
 
-//---ENEMYS
-function Enemys(l, dir, speed){
-    this.left = l;
-    this.timerId;
-    this.direction = dir;
-    this.speed = speed;
+// ---ENEMYS
+function Enemys (l, dir, speed) {
+  this.left = l
+  this.timerId
+  this.direction = dir
+  this.speed = speed
 }
 
 Enemys.prototype.move = function () {
-    var enemy = document.querySelector('#enemy');
+  const enemy = document.querySelector('#enemy')
 
-    if (game.enemy.left > 705 - 28 || game.enemy.left < 260+10) { game.enemy.direction *= -1; }
-    game.enemy.left += 10 * game.enemy.direction;
-    enemy.style.left = game.enemy.left + 'px';
+  if (game.enemy.left > 705 - 28 || game.enemy.left < 260 + 10) { game.enemy.direction *= -1 }
+  game.enemy.left += 10 * game.enemy.direction
+  enemy.style.left = game.enemy.left + 'px'
 
-    if (game.enemy.direction === -1) {
-        enemy.classList.remove("enemy2");
-        enemy.classList.add("enemy1");
-    } else {
-        enemy.classList.remove("enemy1");
-        enemy.classList.add("enemy2");
-    }
+  if (game.enemy.direction === -1) {
+    enemy.classList.remove('enemy2')
+    enemy.classList.add('enemy1')
+  } else {
+    enemy.classList.remove('enemy1')
+    enemy.classList.add('enemy2')
+  }
 }
